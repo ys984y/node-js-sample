0cfc2acdb8559edb67ce651f12914ea0baf07df9
ys984y <114851808+ys984y@users.noreply.github.com>
2023-01-05 12:32:20 +0530

    Create version.ts

---
+++ version.ts
@@ -0,0 +1,29 @@
+export const VERSION = {
+  dirty: true,
+  raw: 'v2.5.9-rc.89-3-ga10f1e046-dirty',
+  hash: 'ga10f1e046',
+  distance: 3,
+  tag: 'v2.5.9-rc.89',
+  semver: {
+    options: {
+      loose: false,
+      includePrerelease: false,
+    },
+    loose: false,
+    raw: 'v2.5.9-rc.89',
+    major: 2,
+    minor: 5,
+    patch: 9,
+    prerelease: ['rc', 89],
+    build: [],
+    version: '2.5.9-rc.89',
+  },
+  suffix: '3-ga10f1e046-dirty',
+  semverString: '2.5.9-rc.89+3.ga10f1e046',
+  version: '2.5.9-rc.90',
+  crux: {
+    components: 'v4.5.9',
+    services: 'v4.0.9',
+    themes: 'v4.0.0',
+  },
+};
